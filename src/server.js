0a1cec079423b34e788ad8ad194046896d4c69b0
Elayne Melo <elayne.mello6@gmail.com>
2023-07-11 21:31:52 -0300

    alterações 2

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import { app } from "./app.js";
 
 app.listen({
     host: '0.0.0.0',
-    port: process.env.PORT ? Number (process.env.PORT) : 3333
+    port: process.env.PORT ? Number (process.env.PORT) : 3333,
 }).then(()=>{
     console.log('Server iniciado.');
 })
